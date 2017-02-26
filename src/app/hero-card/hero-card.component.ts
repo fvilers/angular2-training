873d85f87612b74ccf93a4394598f953658383f5
Fabian Vilers <fabian.vilers@dev-one.com>
2017-02-26 19:06:41 +0100

    Add favorite button and learn more fab

--- src/app/hero-card/hero-card.component.ts
+++ src/app/hero-card/hero-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 
 import { Hero } from '../hero';
 
@@ -10,14 +11,20 @@ import { Hero } from '../hero';
 })
 export class HeroCardComponent implements OnInit {
   @Input() hero: Hero;
+  private snackBarDefaultConfig = new MdSnackBarConfig();
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private snackBar: MdSnackBar) {
+    this.snackBarDefaultConfig.duration = 1500;
   }
 
   ngOnInit() {
   }
 
-  goToDetail () {
+  goToDetail() {
     this.router.navigate(['/heroes', this.hero.name]);
+  }
+
+  addToFavorites() {
+    this.snackBar.open(`✔ ${this.hero.name} has been added to your favorite heroes`, null, this.snackBarDefaultConfig);
   }
 }
