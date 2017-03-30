69692a8ec381bec520677f630b3827135cf1e030
Fabian Vilers <fabian.vilers@dev-one.com>
2017-03-30 08:49:48 +0200

    Add compression

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const compression = require('compression');
+const app = express();
 
+app.use(compression());
 app.use('*', (req, res) => res.sendStatus(404));
 
 module.exports = app;
