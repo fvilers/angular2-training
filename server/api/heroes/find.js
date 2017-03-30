b48974f03a8ba1eb09453889bcc5d1aa74c9edee
Fabian Vilers <fabian.vilers@dev-one.com>
2017-03-30 14:50:24 +0200

    Update find middleware to find hero

--- server/api/heroes/find.js
+++ server/api/heroes/find.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const Hero = require('../../models/hero');
 const createError = require('http-errors');
 
 function findHeroes (req, res, next) {
-  throw new createError.NotImplemented();
+  const query = {}
+  Hero.find()
+    .then(sendResponse)
+    .catch(next)
+  ;
+
+  function sendResponse (heroes) {
+    const list = heroes.map(hero => hero.toObject());
+
+    res.status(200).json(list);
+  }
 }
 
 module.exports = findHeroes;
