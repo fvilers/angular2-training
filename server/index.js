b7eb08bbeca25d6c00c579a5ad618719737a1439
Fabian Vilers <fabian.vilers@dev-one.com>
2017-03-30 10:24:11 +0200

    Add morgan

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const compression = require('compression');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const path = require('path');
 const www = path.join(__dirname, '../dist');
 const app = express();
 
 app.use(compression());
 app.use(helmet());
+app.use(morgan('dev'));
 app.use(express.static(www));
 app.use('*', (req, res) => res.sendFile('index.html', { root: www }));
 
