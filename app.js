c1bd2544dc8afdbf2506635ffad8296777aeb61e
Fabian Vilers <fabian.vilers@dev-one.com>
2017-03-30 08:50:38 +0200

    Add helmet

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const compression = require('compression');
+const helmet = require('helmet');
 const app = express();
 
 app.use(compression());
+app.use(helmet());
 app.use('*', (req, res) => res.sendStatus(404));
 
 module.exports = app;
