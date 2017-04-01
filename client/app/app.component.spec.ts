769d45e40e04cb8c558cef04400714fe40e50d23
Fabian Vilers <fabian.vilers@dev-one.com>
2017-04-01 20:29:37 +0200

    Fix issue with title

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -23,10 +23,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'Angular 2 Training'`, async(() => {
+  it(`should have as title 'The Nexus'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular 2 Training');
+    expect(app.title).toEqual('The Nexus');
   }));
 
 });
