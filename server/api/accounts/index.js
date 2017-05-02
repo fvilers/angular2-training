78d07b9713758d4e3bf04a11e82533b1a9586b64
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-02 10:43:57 +0200

    Add accounts base endpoint

--- server/api/accounts/index.js
+++ server/api/accounts/index.js
@@ -3,7 +3,4 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/accounts', require('./accounts'));
-router.use('/heroes', require('./heroes'));
-
 module.exports = Object.freeze(router);
