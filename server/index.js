28cd395cb137547cb1975e5d11fe663a8e7cac56
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-02 11:13:20 +0200

    Add body parsers

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -15,12 +16,13 @@ const app = express();
 app.use(cors(configuration.cors));
 app.use(compression());
 app.use(helmet());
+app.use(bodyParser.json(configuration.json));
+app.use(bodyParser.urlencoded(configuration.urlencoded));
 app.use(morgan('dev'));
 app.use(express.static(www));
 app.use('/api', require('./api'));
 app.use('*', (req, res) => res.sendFile('index.html', { root: www }));
 app.use((err, req, res, next) => res.status(err.status || 500).send(err));
-
 
 // MongoDB
 mongoose.Promise = global.Promise;
