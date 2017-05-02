1b90ffe00b4026ae30ea3878e77012e974e405aa
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-02 13:54:07 +0200

    Add register method

--- client/app/accounts/services/account.service.ts
+++ client/app/accounts/services/account.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class AccountService {
+  private url = `${environment.backend.url}/accounts`;
 
-  constructor() { }
+  constructor(private http: Http) {
+  }
 
+  register(email: string, password: string): Promise<void> {
+    const data = { email, password };
+
+    return this.http
+      .post(this.url, data)
+      .toPromise()
+      .then(response => {});
+  }
 }
