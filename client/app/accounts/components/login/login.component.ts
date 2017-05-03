9a0eac2c846a865dcd1b4dd61d8495354e09246d
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-03 09:40:24 +0200

    Add the login form

--- client/app/accounts/components/login/login.component.ts
+++ client/app/accounts/components/login/login.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Account } from '../../models';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LoginComponent {
+  public account = new Account();
 }
