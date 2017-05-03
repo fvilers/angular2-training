3a8f23c0dc197494eca12d7a3b5cf706e73ea8ff
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-03 10:10:31 +0200

    Add login method and store jwt

--- client/app/accounts/components/login/login.component.ts
+++ client/app/accounts/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Account } from '../../models';
+import { AccountService } from '../../services';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +11,28 @@ import { Account } from '../../models';
 })
 export class LoginComponent {
   public account = new Account();
+  public errorMessage: string;
+
+  constructor(private service: AccountService, private router: Router) {
+  }
+
+  login() {
+    this.service
+      .createToken(this.account.email, this.account.password)
+      .then(token => {
+        localStorage.setItem('jwt', token);
+        this.router.navigate(['/']);
+      })
+      .catch(error => this.errorMessage = getErrorMessage(error.status))
+    ;
+
+    function getErrorMessage(status) {
+      switch (status) {
+        case 403:
+          return 'This email or password is incorrect.';
+        default:
+          return `We're sorry, but an unexpected error occurred.`;
+      }
+    }
+  }
 }
