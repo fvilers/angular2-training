203be0fd4c7d7fc8ad819888e15c6aff50018036
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-09 16:59:17 +0200

    Add the favorites endpoint

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/accounts', require('./accounts'));
+router.use('/favorites', require('./favorites'));
 router.use('/heroes', require('./heroes'));
 
 module.exports = Object.freeze(router);
