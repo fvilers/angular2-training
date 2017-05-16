c4d5468b26a6f3596f464897490d63326ea1bc1e
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-16 08:22:46 +0200

    Add the favorite property for logged account

--- server/api/heroes/find.js
+++ server/api/heroes/find.js
@@ -1,30 +1,62 @@
 'use strict';
 
+const Account = require('../../models/account');
 const Hero = require('../../models/hero');
 const createError = require('http-errors');
 
 function findHeroes (req, res, next) {
-  const regexp = new RegExp(req.query.terms, 'i');
-  const query = {
-    name: { $regex: regexp }
-  }
-
-  if (req.query.universe || req.query.universe === 0) {
-    query.universe = req.query.universe;
-  }
-
-  if (req.query.role || req.query.role === 0) {
-    query.roles = { $in: [ req.query.role ] };
-  }
-
-  Hero.find(query)
+  getAccount()
+    .then(findHeroes)
+    .then(setFavorites)
     .then(sendResponse)
-    .catch(next);
+    .catch(next)
+  ;
+
+  function getAccount () {
+    if (!req.user) {
+      return Promise.resolve(null);
+    }
+
+    return Account.findOne({ _id: req.user.id });
+  }
+
+  function findHeroes (account) {
+    const regexp = new RegExp(req.query.terms, 'i');
+    const query = {
+      name: { $regex: regexp }
+    }
+
+    if (req.query.universe || req.query.universe === 0) {
+      query.universe = req.query.universe;
+    }
+
+    if (req.query.role || req.query.role === 0) {
+      query.roles = { $in: [ req.query.role ] };
+    }
+    return Promise.all([
+      account,
+      Hero.find(query)
+    ]);
+  }
+
+  function setFavorites (values) {
+    const [account, heroes] = values;
+
+    if (!account) {
+      return heroes;
+    }
+
+    const favorites = account.favorites.map(favorite => favorite.toString());
+
+    return heroes.map(hero => {
+      const result = hero.toObject();
+      result.favorite = favorites.includes(hero.id);
+      return result;
+    });
+  }
 
   function sendResponse (heroes) {
-    const list = heroes.map(hero => hero.toObject());
-
-    res.status(200).json(list);
+    res.status(200).json(heroes);
   }
 }
 
