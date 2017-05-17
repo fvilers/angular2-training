4518a652ba58b1f2d97bf6ed8c4e5ce7a0d5114e
sylvainguerin <sguerin@guess-engineering.com>
2017-05-17 20:53:33 +0200

    unit-tests fixed

--- client/app/accounts/components/register/register.component.spec.ts
+++ client/app/accounts/components/register/register.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComparisonModule } from 'angular-comparison';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
+import { RouterStub } from '../../../core';
+import { MaterialModule } from '@angular/material';
 
 import { RegisterComponent } from './register.component';
+import { AccountService, CurrentAccountService } from '../../services';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,7 +16,19 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        ComparisonModule,
+        FormsModule,
+        HttpModule,
+        MaterialModule
+      ],
+      providers: [
+        AccountService,
+        CurrentAccountService,
+        {provide: Router, useValue: RouterStub }
+      ]
     })
     .compileComponents();
   }));
