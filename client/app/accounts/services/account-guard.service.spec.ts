cc14562949f9dc380e4c63e4174ec5dd4f7c7a05
Fabian Vilers <fabian.vilers@dev-one.com>
2017-05-22 16:48:25 +0200

    Fix unit tests

--- client/app/accounts/services/account-guard.service.spec.ts
+++ client/app/accounts/services/account-guard.service.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
-import { AccountGuardService } from './account-guard.service';
+import { RouterStub } from '../../core';
+import { AccountGuardService, CurrentAccountService } from '.';
 
 describe('AccountGuardService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AccountGuardService]
+      providers: [
+        AccountGuardService,
+        CurrentAccountService,
+        { provide: Router, useValue: RouterStub }
+      ]
     });
   });
 
